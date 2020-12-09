b85578db2aca9947a093f2eba819257373f14383
MFx <35132814+stranger7mt@users.noreply.github.com>
2020-12-09 09:56:28 +0700

    Add project files.

---
+++ C Sharp Fundamental/BASIC/unit1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace C_Sharp_Fundamental.BASIC
+{
+    class unit1
+    {
+        public static void Main(string [] args ) 
+        {
+            Console.Title = "First program";
+            Console.ReadKey();
+            Console.WriteLine(" hello world !");
+        }
+    }
+}
